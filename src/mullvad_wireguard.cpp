8f946839be638b8d4c249ce333a15d3e973eab61
Love Billenius <lovebillenius@disroot.org>
2025-02-04 16:52:45 +0100

    mullvad wg

---
+++ src/mullvad_wireguard.cpp
@@ -0,0 +1,65 @@
+#include "mullvad_wireguard.hpp"
+#include "spdlog/spdlog.h"
+
+
+MullvadWireGuard::MullvadWireGuard(std::string config_file) :
+    m_is_connected(false), m_config_file(std::move(config_file)), m_interface_name(get_interface_name(config_file)),
+    m_vpn_os_specific(get_vpn_os_specific()) {
+    start();
+}
+
+MullvadWireGuard::~MullvadWireGuard() {
+    if (is_connected())
+        close();
+}
+
+
+void MullvadWireGuard::start() {
+    spdlog::info("Starting WireGuard connection...");
+    execute_command("wg-quick up " + m_config_file);
+    int counter = 0;
+    while (!is_connected()) {
+        if (++counter > 10) {
+            close();
+            throw std::runtime_error("WireGuard connection timeout");
+        }
+        sleep(2);
+    }
+    m_is_connected = true;
+    spdlog::info("WireGuard connection established.");
+}
+
+void MullvadWireGuard::close() {
+    spdlog::info("Stopping WireGuard connection...");
+    try {
+        execute_command("wg-quick down " + m_config_file);
+        m_is_connected = false;
+    } catch (const std::exception &e) {
+        spdlog::error(e.what());
+    }
+}
+
+bool MullvadWireGuard::is_connected() const { return m_vpn_os_specific->is_connected(m_interface_name); }
+
+
+std::string MullvadWireGuard::get_interface_name(const std::string &path) {
+    const std::filesystem::path p(path);
+    return p.stem().string();
+}
+
+void execute_command(const std::string &cmd) {
+    spdlog::debug("Executing: " + cmd);
+    const std::string full_cmd = cmd + " 2>&1";
+    FILE *pipe = popen(full_cmd.c_str(), "r");
+    if (!pipe)
+        throw std::runtime_error("Failed to execute command");
+
+    char buffer[128];
+    std::string result;
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr)
+        result += buffer;
+
+    int status = pclose(pipe);
+    if (WEXITSTATUS(status) != 0)
+        throw std::runtime_error("Command failed: " + result);
+}
---
+++ src/mullvad_wireguard.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "vpn_os_specific.hpp"
+
+#include <filesystem>
+
+class MullvadWireGuard {
+public:
+    MullvadWireGuard(std::string config_file);
+
+    ~MullvadWireGuard();
+
+    void start();
+
+    void close();
+
+    [[nodiscard]] bool is_connected() const;
+
+private:
+    static std::string get_interface_name(const std::string &path);
+
+    void execute_command(const std::string &cmd);
+
+private:
+    bool m_is_connected;
+    std::string m_config_file;
+    std::string m_interface_name;
+    std::unique_ptr<VpnOsSpecific> m_vpn_os_specific;
+};
