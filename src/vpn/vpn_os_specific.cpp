0148f71d32023c333b3aa900af2e3f7f9495670b
Love Billenius <lovebillenius@disroot.org>
2025-02-12 14:49:43 +0100

    Fix linuxvpnosspecificv

--- src/vpn/vpn_os_specific.cpp
+++ src/vpn/vpn_os_specific.cpp
@@ -1,12 +1,32 @@
 #include "vpn_os_specific.hpp"
 
+#include <array>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+std::string exec(const char *cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe)
+        throw std::runtime_error("popen() failed!");
+    while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
 namespace vpn {
 
 class LinuxVpnOsSpecific final : public VpnOsSpecific {
 public:
     [[nodiscard]] bool is_connected(const std::string &interface_name) const override {
-        const std::string cmd = "ip link show " + interface_name + " | grep -q 'state UP'";
-        return system(cmd.c_str()) == 0;
+        const std::string cmd = "ip addr show " + interface_name;
+        const std::string ret = exec(cmd.c_str());
+        return ret.find("inet ") != std::string::npos;
     }
 };
 
